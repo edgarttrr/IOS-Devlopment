f4e5a4125efb170704626a81bd6f709c0523176c
edgarttrr <Edgarttrr@gmail.com>
2018-03-03 13:09:26 -0800

    Updating repo for new file

---
+++ SlotMachineFinal/Slot Machine/Slot Machine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Slot Machine
+//
+//  Created by edgar t rendon on 3/3/18.
+//  Copyright © 2018 edgar t rendon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
